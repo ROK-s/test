b6e50954d4a84c681f884da3887f30e0466654a4
ROK-s <53135860+ROK-s@users.noreply.github.com>
2019-12-08 22:38:54 +0900

    fulid clock
    add setInterval
    add 01,02,03.......

--- clock.js
+++ clock.js
@@ -6,11 +6,14 @@ const clockClick = document.querySelector(".js-clock"),
             const minutes = date.getMinutes();
             const hours = date.getHours();
             const seconds = date.getSeconds();
-            clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+            clockTitle.innerText = `${hours < 10 ? `0${seconds}` : seconds}:
+                                    ${minutes < 10 ? `0${seconds}` : seconds}:
+                                    ${seconds < 10 ? `0${seconds}` : seconds}`;
         }
         
         function init() {
             getTime();
+            setInterval(getTime, 1000);
         }
         
         init();
